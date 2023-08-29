182c9bb9a272e45213947a52dc3bdeb2c1e8e247
Haroon <haroonhakimi2005@gmail.com>
2023-08-29 16:58:27 -0400

    Worked on the searchbar, added a footer, removed movies.html

--- app.js
+++ app.js
@@ -1,36 +1,43 @@
-const moviesList = document.querySelector(`.movies`);
 const API_KEY = "5c0a8d0";
 let search = "";
-
-// async function movieSearch()
-// {
-//     const movies = await fetch(`https://www.omdbapi.com/?apikey=${API_KEY}&s=${search}`)
-//     const moviesData = await movies.json()
-//     console.log(moviesData)
-//     moviesList.innerHTML = [moviesData].map((movie) => movieHTML(movie))
-// }
-
-// movieSearch()
+const moviesWrapper = document.querySelector(`.movies`)
 
 function onSearchChange(event)
 {
     const search = event.target.value
     localStorage.setItem("search", search)
-    openMovies(search)
+    window.location.href = `${window.location.origin}/movies.html`
 }
 
-function openMovies()
+function bringUserHome()
 {
-    window.localStorage.href = `${window.location.origin}/movies.html`
+    window.location.href = `${window.location.origin}/index.html`;
 }
 
-// function movieHTML(movie)
-// {
-//    return `<li class="popular__movie movie">
-//         <figure class="movie__img--wrapper">
-//             <img src="${movie.Poster}" class="movie__img" alt="">
-//         </figure>
-//         <h2 class="popular__movie--title movie__title">${movie.Title}</h2>
-//     </li>`
-// }
+
+async function movieSearch()
+{
+    const movies = await fetch(`https://www.omdbapi.com/?apikey=${API_KEY}&s=${search}`)
+    const moviesData = await movies.json()
+    console.log(moviesData)
+    
+}
+
+setTimeout(() => movieSearch())
+
+function renderBooks(movie)
+{
+    const moviesHTML = moviesData.map(movie => 
+        {
+
+        return `<li class="popular__movie movie">
+                    <figure class="movie__img--wrapper">
+                        <img src="${movie.Poster}" class="movie__img" alt="">
+                    </figure>
+                    <h2 class="popular__movie--title movie__title">${movie.Title}</h2>
+                </li>`
+        })
+    console.log(moviesHTML)
+    moviesWrapper.innerHTML = moviesHTML
+}
 
