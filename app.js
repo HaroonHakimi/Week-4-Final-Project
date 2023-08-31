4b960e6a74d0293acedb95e79080aaec86273dbd
Haroon <haroonhakimi2005@gmail.com>
2023-08-31 17:36:52 -0400

    Fixed hovering on the links, reorganized app.js

--- app.js
+++ app.js
@@ -1,31 +1,32 @@
-let search = "";
-const moviesWrapper = document.querySelector(`.movies`)
-moviesWrapper.classList += (` movies__loading`)
-moviesWrapper.classList.remove(`movies__loading`)
+let search= ""
+const moviesWrapper = document.querySelector(`.movies`);
+moviesWrapper.classList += ` movies__loading`;
+moviesWrapper.classList.remove(`movies__loading`);
+const searchInput = document.querySelector(`.search__input`);
+const loadingSpinner = document.querySelector(`.movies__loading`);
+loadingSpinner.style.display = `none`;
 
 
 async function renderMovies(filter)
 {
-    const searchInput = document.querySelector(`.search__input`)
-    const query = searchInput.value
+    const query = searchInput.value;
 
     moviesWrapper.classList.add(`movies__loading`)
-    setTimeout(1000)
 
     const movies = await fetch(`https://www.omdbapi.com/?apikey=5c0a8d0&s=${query}`)
     const moviesData = await movies.json()
-    const moviesArray = moviesData.Search;
-
-    moviesWrapper.innerHTML = moviesArray.map((movie) => getMoviesHTML(movie)).join("")
-
+    const moviesArray = moviesData.Search.slice(0,6);
+    
     if (filter === `NEW_TO_OLD`)
     {
         moviesArray.sort((a,b) => b.Year - a.Year)
     }
-    else (filter === `OLD_TO_NEW`)
+    if (filter === `OLD_TO_NEW`)
     {
         moviesArray.sort((a,b) => a.Year - b.Year)
     }
+
+    moviesWrapper.innerHTML = moviesArray.map((movie) => getMoviesHTML(movie)).join("")
 
     moviesWrapper.classList.remove("movies__loading");
 }
@@ -36,7 +37,7 @@ function filterMovies(event)
 }
 
 
-function getMoviesHTML()
+function getMoviesHTML(movie)
 {
     return `<div class="popular__movie movie">
     <figure class="movie__img--wrapper">
